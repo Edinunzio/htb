f4475ce4aaa5e3c5a9afa43c7bef8bda5993bab3
Edinunzio <e.dinunzio@gmail.com>
2023-04-20 10:23:22 -0400

    hiding blue checks, whitelist not enabled.

--- src/content_script.js
+++ src/content_script.js
@@ -1,19 +1,23 @@
-// Array of Twitter account usernames to exclude from hiding
-const excludedAccounts = ['username1', 'username2'];
+// TODO: Array of Twitter account usernames to exclude from hiding
+// const excludedAccounts = ['username1', 'username2'];
 
 function hideVerifiedTweets() {
-  const verifiedAccounts = document.querySelectorAll('.css-1dbjc4n.r-18u37iz.r-1wbh5a2 .css-901oao.r-1fmj7o5.r-6koalj.r-1qd0xha.r-a023e6.r-b88u0q.r-16dba41.r-1h0z5md.r-ad9z0x.r-bcqeeo .css-18t94o4:not([hidden]) .css-901oao .css-16my406');
   
-  for (let account of verifiedAccounts) {
-    const username = account.innerText.trim();
-    if (excludedAccounts.includes(username)) {
-      continue; // Skip excluded accounts
+    // Get all 'article' elements in the document
+    var articles = document.getElementsByTagName("article");
+    console.log(articles);
+  
+    // Loop through each 'article' element
+    for (let i = 0; i < articles.length; i++) {
+      // Check if the article contains an 'svg' element
+      const containsSVG = articles[i].querySelector('svg.r-1cvl2hr') !== null;
+  
+      // If the article contains an 'svg' element, hide it
+      if (containsSVG) {
+        articles[i].style.display = 'none';
+      }
     }
-    
-    const tweet = account.closest('.css-1dbjc4n.r-18u37iz.r-1wbh5a2 .css-1dbjc4n.r-18u37iz.r-1wbh5a2 .css-1dbjc4n.r-18u37iz.r-1wbh5a2');
-    tweet.style.display = 'none';
   }
-}
-
+  
 window.setInterval(hideVerifiedTweets, 500);
-
+  
