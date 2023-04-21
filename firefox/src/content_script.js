d95a1393956941fdbbd123df01f3b9a11060d1c5
Edinunzio <e.dinunzio@gmail.com>
2023-04-21 16:38:38 -0400

    removing user.

--- firefox/src/content_script.js
+++ firefox/src/content_script.js
@@ -1,5 +1,5 @@
 // Array of Twitter account usernames to exclude from hiding
-var excludedAccounts = ['StephenKing', 'KingJames', 'jpohhhh '];
+var excludedAccounts = ['StephenKing', 'KingJames'];
 
 function hideVerifiedTweets() {
 
