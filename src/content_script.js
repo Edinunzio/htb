209e6ee92b03550459f6b1e8466a9e2e31ca09e5
Edinunzio <e.dinunzio@gmail.com>
2023-04-21 14:41:16 -0400

    excluding Stephen King and Lebron James

--- src/content_script.js
+++ src/content_script.js
@@ -1,10 +1,7 @@
-// TODO: Array of Twitter account usernames to exclude from hiding
-var excludedAccounts = ['StephenKing', 'username2'];
+// Array of Twitter account usernames to exclude from hiding
+var excludedAccounts = ['StephenKing', 'KingJames'];
 
 function hideVerifiedTweets() {
-  
-    // Get all 'article' elements in the document
-    //var articles = document.getElementsByTagName("article");
 
     const articles = document.querySelectorAll('article'); // select all article elements
     const anchors = []; // create an empty array to store all the anchor tags
@@ -17,64 +14,20 @@ function hideVerifiedTweets() {
           articleIndex: articleIndex,
           anchorIndex: anchorIndex
         };
-        var blah = String(anchor);
-        if (!blah.match("StephenKing")){
+        var remove = true;
+        for(let i=0; i<excludedAccounts.length; i++){
+          var tweet = String(anchor);
+          if (tweet.match(excludedAccounts[i])){
+            remove = false;
+          }  
+        }
+        if (remove){
           anchors.push(anchorObject);
         }
-        
       });
     });
 
-    //console.log(anchors)
-    for (let i=0; i < anchors.length; i++){
-      if (anchors[i]["anchor"]["href"].includes("StephenKing")){
-      }
-      //console.log(anchors[i]["anchor"]["href"]);
-    }
-
-
-    //var user_links = document.getElementsByTagName('article a');
-    //console.log(user_links);
-    //var articles = [];
-    /* for (let i = 0; i < excludedAccounts.length; i++) {
-      for (let i = 0; i < user_links.length; i++) {
-        console.log(user_links[i]);
-
-        //console.log(excludedAccounts[i]);
-        if (user_links[i].toString().includes(`/${excludedAccounts[i]}`)) {
-          console.log(excludedAccounts[i] + 'found');
-        } else {
-          console.log(excludedAccounts[i] + 'not found');
-          articles.push(_articles[i]);
-        }
-      }
-    } */
-  
-
-  
-    // Loop through each 'article' element
-    // for (let i = 0; i < articles.length; i++) {
-    //   /*for (let excludedAccount of excludedAccounts) {
-    //     if (articles[i].includes(excludedAccount)) {
-    //       articles.pop(i); // Skip included accounts
-    //     }
-    //   }*/
-    //   // Check if the article contains an 'svg' element
-    //   const containsSVG = articles[i].querySelector('svg.r-1cvl2hr') !== null;
-  
-    //   // If the article contains an 'svg' element, hide it
-    //   if (containsSVG) {
-    //     articles[i].style.display = 'none';
-    //   }
-    // }
-
     for (let i = 0; i < anchors.length; i++) {
-      /*for (let excludedAccount of excludedAccounts) {
-        if (articles[i].includes(excludedAccount)) {
-          articles.pop(i); // Skip included accounts
-        }
-      }*/
-      console.log(anchors);
       // Check if the article contains an 'svg' element
       const containsSVG = anchors[i]["anchor"].querySelector('svg.r-1cvl2hr') !== null;
   
@@ -83,11 +36,7 @@ function hideVerifiedTweets() {
         articles[anchors[i]["articleIndex"]].style.display = 'none';
       }
     }
-
-
-
-
   }
   
-window.setInterval(hideVerifiedTweets, 5000);
+window.setInterval(hideVerifiedTweets, 500);
   
